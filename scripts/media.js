267689dad4b3980e04a9640eefbf46caef05bff6
DuvanVilladiego <Duvan.villadiego.oficial@gmail.com>
2021-02-22 11:31:37 -0500

    bost

--- scripts/media.js
+++ scripts/media.js
@@ -1,21 +1,15 @@
 //objeto para llamar a la camara
- var x = true;
-if (x == true) {
-   function getStreamAndRecord() {
-      navigator.mediaDevices.getUserMedia({
-         audio: false,
-         video: {
-            height: { max: 480 }
-         }
-      })
-         .then(function (stream) {
-            video.srcObject = stream;
-            video.play()
-         })
-   }
-   x = false;
-} else {
-   mediaStream.getVideoTracks()[0].stop();
-   x = true;
-}
 
+function getStreamAndRecord() {
+   navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+         height: { max: 480 }
+      }
+   })
+      .then(function (stream) {
+         video.srcObject = stream;
+
+         video.play()
+      })
+}
