2fe879ccd1bc67d7674c82e5b47b6c2716e357e8
Mario Jose Rodriguez Machacon <75395682+mrodriguezm21@users.noreply.github.com>
2021-07-01 17:03:10 -0500

    feat: clear input with button css

--- scripts/suggestions.js
+++ scripts/suggestions.js
@@ -1,9 +1,14 @@
 async function suggestions() {
-  const input = document.getElementById("searchbox").value;
+  const input = document.getElementById("searchbox");
   const sug = document.querySelector(`#suggestions`);
+  const clearbutton = document.getElementById("clearbutton");
+  const searchButton = document.getElementById("search-icon")
 
-  if (input == null || input.length < 2) {
+  if (input.value == null || input.value.length < 2) {
+    clearbutton.style.display = "none";
     sug.style.display = "none";
+  searchButton.style.display = "block"
+
     return false;
   }
   sug.style.display = "flex";
@@ -17,23 +22,22 @@ async function suggestions() {
     let show = document.querySelector(`.suggestions .sg-${i}`);
     show.innerHTML = data[i].name;
   }
-}
 
-let elementos = document.querySelectorAll(".suggestions span");
-const input = document.getElementById("searchbox");
-elementos.forEach((span) => {
-  const input = document.getElementById("searchbox");
+  let elementos = document.querySelectorAll(".suggestions span");
+  elementos.forEach((span) => {
+    span.onclick = function () {
+      document.getElementsByTagName("span").value = this.innerHTML;
+      input.value = this.innerHTML;
+      test = 9;
+      getGifs(input.value);
+    };
+  });
 
-  span.onclick = function () {
-    document.getElementsByTagName("span").value = this.innerHTML;
-    input.value = this.innerHTML;
-    test=9;
-    getGifs(input.value);
+  clearbutton.style.display = "flex";
+  searchButton.style.display = "none"
+  clearbutton.onclick = function () {
+    input.value = "";
+    sug.style.display = "none";
+    clearbutton.style.display = "none";
   };
-});
-
-
-const clearbutton = document.getElementById("clearbutton")
-clearbutton.onclick = function(){
-  input.value = ""
 }
